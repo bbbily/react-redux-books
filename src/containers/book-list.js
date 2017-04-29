7d5189e2e72fbf32440b50fa63c679507344b82a
Peng Wu <peng116233402@gmail.com>
2017-04-29 15:18:14 -0600

    add book_detail component

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import { selectBook } from "../actions/index";
 
 class BookList extends Component {
   renderList() {
-    return this.props.books.map(book => <li key={ book.title }>{ book.title }</li>)
+    return this.props.books.map(book => <li onClick={ () => this.props.dispatch(selectBook(book)) } key={ book.title }>{ book.title }</li>)
   }
 
   render() {
